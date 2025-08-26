e57c15f4d4eed120a2f8f4760369befb28895829
MuhammedArdaZ <zmuhammedarda@gmail.com>
2025-08-26 16:23:44 +0300

    feat: user password crypted, logout session fixed

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var express = require("express");
-var app = express();
-var session = require("express-session");
-var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
-var fs = require("fs");
-var { v4: uuidv4 } = require("uuid");
-var bcrypt = require('bcrypt');
-const { error } = require("console");
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcrypt");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -14,11 +13,9 @@ app.use(cookieParser());
 app.use(session({
     secret: "sample-secret",
     resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+    saveUninitialized: false
 }));
-
-app.use(express.static("public"));
+app.use(express.static("public"))
 
 function readNewsDB() {
     return JSON.parse(fs.readFileSync(("newsDB.json")));
@@ -38,17 +35,8 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/public/main-page.html");
 })
 
-//
-app.get("/api/register", (req, res) => {
-    res.send("Register sayfası sadece POST ile çalışır.");
-});
-
-
 app.post("/api/register", async function (req, res) {
-    let usersDatabase = await readUsersDB();
-
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
+    const usersDatabase = await readUsersDB();
     if (usersDatabase.find((user) => user.email === req.body.email)) {
         res.json({ message: "This email has been taken." });
     }
@@ -56,6 +44,7 @@ app.post("/api/register", async function (req, res) {
         res.json({ message: "Passwords do not match!" });
     }
     else {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
         const newUser = {
             id: uuidv4(),
             name: req.body.name,
@@ -63,43 +52,37 @@ app.post("/api/register", async function (req, res) {
             email: req.body.email,
             password: hashedPassword,
             authenticationLevel: 0,
+            postedNews: [],
+            comments: [],
             isLoggedIn: false
         }
-
         usersDatabase.push(newUser);
         writeUsersDB(usersDatabase);
-        res.redirect("/");
+        return res.redirect("/main-page.html");
     }
 })
 
 app.post("/api/login", async function (req, res) {
     const userDatabase = readUsersDB();
-    const user = userDatabase.find(function (user) { return user.email === req.body.email });
-
-    if (user) {
-
-        const isMatch = await bcrypt.compare(req.body.password, user.password);
-
-        if (isMatch == false) {
-            return res.send('Wrong password!');
+    const userIndex = userDatabase.findIndex(function (user) { return user.email === req.body.email });
+    if (userIndex !== -1) {
+        let user = userDatabase[userIndex];
+        const comparePassword = await bcrypt.compare(req.body.password, user.password)
+        if (!comparePassword) {
+            return res.send("Password is wrong!");
         }
-
         else {
-            const userIndex = userDatabase.findIndex(u => u.email === req.body.email);
-            userDatabase[userIndex].isLoggedIn = true; // Veritabanında güncelle
+            user.isLoggedIn = true;
             writeUsersDB(userDatabase);
-
             req.session.user = {
-                id: user.id, email: user.email, name: user.name, surname: user.surname,
-                authenticationLevel: user.authenticationLevel, isLoggedIn: user.isLoggedIn
+                id: user.id, email: user.email, name: user.name, surname: user.surname, authenticationLevel: user.authenticationLevel,
+                postedNews: user.postedNews, comments: user.comments, isLoggedIn: true
             };
-            req.session.user.isLoggedIn = true;
             req.session.save();
-
+            console.log(req.session.user); //////////////////////////////
             return res.redirect("/");
         }
     }
-
     else {
         return res.send("This account is not available.");
     }
@@ -112,16 +95,17 @@ app.post("/api/upload-new", async function (req, res) {
             url: url,
             encoding: null
         })
-
         const uploadNewData = {
             id: uuidv4(),
             title: req.body.title,
             content: req.body.content,
             image: req.body.image,
+            category: req.body.category,
+            comments: [],
+            views: 0,
             createdAt: new Date().toISOString(),
             author: req.session.user.id
         }
-
         let newsDatabase = await readNewsDB();
         newsDatabase.push(uploadNewData);
         writeNewsDB(newsDatabase);
@@ -136,28 +120,38 @@ app.post("/api/logout", function (req, res) {
         const userDatabase = readUsersDB();
         const userIndex = userDatabase.findIndex(user => user.id === req.session.user.id);
         if (userIndex !== -1) {
-            userDatabase[userIndex].isLoggedIn = false;
+            userDatabase[userIndex].isLoggedIn = false; 
             writeUsersDB(userDatabase);
         }
         req.session.destroy();
     }
-    res.redirect("/");
-});
+    res.redirect("/main-page.html");
+})
+
 
 app.get("/api/currentUser", function (req, res) {
-    if (req.session.user) {
+    console.log(req.session.user + "___CurrentUSER"); //////////////////////////////
+    if (req.session.user !== undefined) {
         const userDatabase = readUsersDB();
         const user = userDatabase.find((user) => user.id === req.session.user.id);
-
         if (user) {
             return res.json({
-                id: user.id, email: user.email, name: user.name, surname: user.surname,
-                authenticationLevel: user.authenticationLevel, isLoggedIn: user.isLoggedIn
+                id: user.id, email: user.email, name: user.name, surname: user.surname, authenticationLevel: user.authenticationLevel,
+                comments: user.comments, postedNews: user.postedNews, isLoggedIn: user.isLoggedIn
             });
         }
         return res.json(null);
     }
     return res.json(null);
-})
+})  
+
+app.get("/api/news/:id", function (req, res) {
+    const newsDatabase = readNewsDB();
+    const newsData = newsDatabase.find((news) => news.id === req.params.id);
+    if (!newsData) {
+        return res.status(404).json({ error: "News not found" })
+    };
+    res.json(newsData);
+});
 
 app.listen(3000);
