194eabf1a833d4c870a58d530ba22e4b438c0fee
MuhammedArdaZ <zmuhammedarda@gmail.com>
2025-08-27 12:21:34 +0300

    refactor: html to pug

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
 
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser());
@@ -32,7 +35,11 @@ function writeUsersDB(data) {
 
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/public/main-page.html");
+    res.render("main-page", { user: req.session.user });
+})
+
+app.get("register", function (req, res) {
+    res.render("register-page");
 })
 
 app.post("/api/register", async function (req, res) {
@@ -58,8 +65,12 @@ app.post("/api/register", async function (req, res) {
         }
         usersDatabase.push(newUser);
         writeUsersDB(usersDatabase);
-        return res.redirect("/main-page.html");
+        return res.render("main-page", { user: req.session.user, message: "Your account has been created successfully." });
     }
+})
+
+app.get("/login", function (req, res) {
+    res.render("login-page");
 })
 
 app.post("/api/login", async function (req, res) {
@@ -78,14 +89,20 @@ app.post("/api/login", async function (req, res) {
                 id: user.id, email: user.email, name: user.name, surname: user.surname, authenticationLevel: user.authenticationLevel,
                 postedNews: user.postedNews, comments: user.comments, isLoggedIn: true
             };
-            req.session.save();
-            console.log(req.session.user); //////////////////////////////
-            return res.redirect("/");
+            req.session.save(() => {
+                return res.render("main-page", {
+                    user: req.session.user
+                });
+            });
         }
     }
     else {
         return res.send("This account is not available.");
     }
+})
+
+app.get("/upload", function (req, res) {
+    res.render("new-page", { user: req.session.user });
 })
 
 app.post("/api/upload-new", async function (req, res) {
@@ -120,17 +137,16 @@ app.post("/api/logout", function (req, res) {
         const userDatabase = readUsersDB();
         const userIndex = userDatabase.findIndex(user => user.id === req.session.user.id);
         if (userIndex !== -1) {
-            userDatabase[userIndex].isLoggedIn = false; 
+            userDatabase[userIndex].isLoggedIn = false;
             writeUsersDB(userDatabase);
         }
         req.session.destroy();
     }
-    res.redirect("/main-page.html");
+    res.render("main-page", { user: null, message: "You have logged out successfully." });
 })
 
 
 app.get("/api/currentUser", function (req, res) {
-    console.log(req.session.user + "___CurrentUSER"); //////////////////////////////
     if (req.session.user !== undefined) {
         const userDatabase = readUsersDB();
         const user = userDatabase.find((user) => user.id === req.session.user.id);
@@ -143,13 +159,13 @@ app.get("/api/currentUser", function (req, res) {
         return res.json(null);
     }
     return res.json(null);
-})  
+})
 
 app.get("/api/news/:id", function (req, res) {
     const newsDatabase = readNewsDB();
     const newsData = newsDatabase.find((news) => news.id === req.params.id);
     if (!newsData) {
-        return res.status(404).json({ error: "News not found" })
+        return res.status(404).json({ error: "News not found" });
     };
     res.json(newsData);
 });
